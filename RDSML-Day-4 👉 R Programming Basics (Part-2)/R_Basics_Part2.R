76a85124acebce2e8c56a905e6bccd795e4fe67f
mdkhurshidalam014 <mdkhurshidalam014@gmail.com>
2025-05-10 09:47:31 +0530

    Day 4_Part2_Basic

---
+++ RDSML-Day-4 👉 R Programming Basics (Part-2)/R_Basics_Part2.R
@@ -0,0 +1,55 @@
+#Data_types_in_R
+#1. Numeric : Integer/Floating Point
+#2. String/Character: "Md_Khurshid_Alam"
+#3. Logical
+
+a=11
+b=7.5
+class(a)
+class(b)
+
+name= 'Md Khurshid Alam'
+print(name)
+class(name)
+print(b+8)
+
+#logical_data_types-TRUE, FALSE/T,Fa
+a>b
+class(a>b)
+print(a<b)
+
+x=19
+y=22-3
+print(x==y)
+print(x!=y)
+print(y>x)
+print(y!=y)
+print(y>=x)
+
+
+#Logical_Operators_in_R
+#&&_Logical_Operator 
+4==4 && 5==5
+# ||(or)_logical_operator
+x!=y ||x==y
+
+#Simple_mathematical_operators_in_R
+z=6+9-8*2/3
+print(z)
+z=6+9-8*(2/3)
+w=4+3-3*2/3
+w=4+3-3*(2/3)
+print(w)
+w=(4+3-3)*2/3
+print(w)
+
+
+y=4^2
+print(y)
+
+#for_quotient
+11/5
+#for_remainders
+11%%5
+#for_rounded_value
+11%/%5
