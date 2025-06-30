1d7f4ecfa94e1f6a559da58ee66edda251e931ff
mdkhurshidalam014 <mdkhurshidalam014@gmail.com>
2025-07-01 00:46:23 +0530

    Create Error Bars in R (Part-1).R

---
+++ RDSML-Day-17 👉 Error Bars in R (Part-1)/Error Bars in R (Part-1).R
@@ -0,0 +1,17 @@
+#importing the dataset
+tooth_data= ToothGrowth
+
+#creating a function for summerizing the data
+#based on the grouping variables
+
+data_summary = function(data, varname, groupnames){
+  require(plyr)
+  summary_func = function(x, col){
+    c(mean = mean(x[[col]], na.rm = TRUE), sd = sd(x[[col]], na.rm = TRUE))
+  }
+  data_sum = ddply(data, groupnames, .fun = summary_func, varname)
+  data_sum = rename(data_sum, c("mean" = varname))
+  return(data_sum)
+}
+
+d_summary=data_summary(tooth_data,varname ="len", groupnames = c("supp","dose"))
